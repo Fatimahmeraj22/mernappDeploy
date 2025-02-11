c64bc02117e3541a3c69ba7ea24ed7bd2e9bd29a
Mac <mac@Meraj.local>
2025-02-12 00:30:28 +0530

    mernappDeploy

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 5008;
+
+app.use(express.static(path.join(__dirname, "dist"))); 
+
+app.get("/fatimah", (req, res) => {
+  try {
+    res.status(200).json({ msg: "Hello world!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: error });
+  }
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Running at ${PORT}`);
+});
